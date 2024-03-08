058e34937b90a7bb261f794361d26a53bba54ab1
Murilo Franceschetto <gfmurilogf@gmail.com>
2024-03-08 12:06:02 +0000

    feat: status 200

--- app/main.js
+++ app/main.js
@@ -1,14 +1,15 @@
 const net = require("net");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
-// Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
-    socket.end();
-    server.close();
-  });
+    socket.on("close", () => {
+        socket.end();
+        server.close();
+    });
+    socket.on("data", (data) => {
+        socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        socket.end();
+    });
 });
 
 server.listen(4221, "localhost");
+console.log("Server listening on port 4221")
