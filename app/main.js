e4c31f08fdffce01c817f02e65ff58f4b8b10eef
Murilo Franceschetto <gfmurilogf@gmail.com>
2024-03-08 17:30:10 +0000

    feat: post file

--- app/main.js
+++ app/main.js
@@ -15,10 +15,10 @@ const filesPath = RegExp('^\/files\/(.+)$');
 const userAgentPath = RegExp('^\/user-agent$');
 
 const allowedPaths = [
-    defaultPath, 
-    echoPath,
-    userAgentPath,
-    filesPath,
+    {allowedPath: defaultPath, allowedMethods: ['GET']}, 
+    {allowedPath: echoPath, allowedMethods: ['GET']}, 
+    {allowedPath: userAgentPath, allowedMethods: ['GET']}, 
+    {allowedPath: filesPath, allowedMethods: ['GET', 'POST']},
 ];
 
 function getInfoHeaders(request, matchRegex) {
@@ -35,20 +35,27 @@ function addResponseFile(socket, fileContentBuffer) {
     socket.write(`Content-Length: ${fileContentBuffer.byteLength}\r\n\r\n${fileContentBuffer}`);
 }
 
-function addResponseTextBody(socket, content) {
-    socket.write("HTTP/1.1 200 OK\r\n");
+function addResponseTextBody(socket, content, status = 200) {
+    socket.write(`HTTP/1.1 ${status} OK\r\n`);
     socket.write("Content-Type: text/plain\r\n");
     socket.write(`Content-Length: ${Buffer.byteLength(content)}\r\n\r\n${content}`);
 }
 
 async function verifyFile(filePath) {
     let fullPath = path.join(directory, filePath);
-    console.log(`Buscando arquivo: ${fullPath}`)
+    console.log(`Buscando arquivo: ${fullPath}`);
     if (fs.existsSync(fullPath)) {
         return fs.readFileSync(fullPath);
     }
     return null; 
-} 
+}
+
+async function writeFile(filePath, fileContent) {
+    console.log(filePath, fileContent)
+    let fullPath = path.join(directory, filePath);
+    console.log(`Salvando arquivo: ${fullPath}`);
+    fs.writeFileSync(filePath, fileContent)
+}
 
 async function error404(socket) {
     socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
@@ -56,7 +63,6 @@ async function error404(socket) {
 }
 
 const server = net.createServer((socket) => {
-    
     socket.on("close", () => {
         socket.end();
         server.close();
@@ -67,24 +73,32 @@ const server = net.createServer((socket) => {
         const requestInfo = data.toString('utf8');
         const [method, path, httpVersion] = requestInfo.trim().split(' ');
 
-        if (!allowedPaths.some(allowedPath => allowedPath.test(path))) {
+        let pathConfig = allowedPaths.find(({allowedPath, allowedMethods}) => allowedPath.test(path) && allowedMethods.includes(method));
+        if (!pathConfig) {
             error404(socket);
             return;
         }
 
         const host = getInfoHeaders(requestInfo, /^Host: (.+)$/m);
         const userAgent = getInfoHeaders(requestInfo, /^User-Agent: (.+)$/m);
+        const bodyMessage = requestInfo.split('\r\n\r\n')[1];
 
         const filesMatch = path.match(filesPath);
         const echoMatch = path.match(echoPath);
 
         if (filesMatch) {
-            fileContent = await verifyFile(filesMatch[1]);
-            if (!fileContent) {
-                error404(socket);
-                return;
+            if (method === 'GET') {
+                fileContent = await verifyFile(filesMatch[1]);
+                if (!fileContent) {
+                    error404(socket);
+                    return;
+                }
+                addResponseFile(socket, fileContent);
             }
-            addResponseFile(socket, fileContent);
+            if (method === 'POST') {
+                await writeFile(filesMatch[1], bodyMessage);
+                addResponseTextBody(socket, '', 201);
+            }
 
         } else if (echoMatch) {
             const content = echoMatch[1];
