2fd399aae2e9e173998d47f15844939f4022816a
Murilo Franceschetto <gfmurilogf@gmail.com>
2024-03-08 12:54:31 +0000

    feat: /echo/something route

--- app/main.js
+++ app/main.js
@@ -1,5 +1,13 @@
 const net = require("net");
 
+const defaultPath = RegExp('^\/$');
+const echoPath = RegExp('^\/echo\/.+$');
+
+const allowedPaths = [
+    defaultPath, 
+    echoPath,
+];
+
 const server = net.createServer((socket) => {
     socket.on("close", () => {
         socket.end();
@@ -8,13 +16,22 @@ const server = net.createServer((socket) => {
     socket.on("data", (data) => {
         const requestInfo = data.toString('utf8');
         const [, path] = requestInfo.trim().split(' ');
-        if (path === '/') {
-            socket.write("HTTP/1.1 200 OK\r\n\r\n");
-        } else {
+
+        if (!allowedPaths.some(allowedPath => path.match(allowedPath))) {
             socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+            socket.end();
+            return;
         }
+
+        socket.write("HTTP/1.1 200 OK\r\n");
+
+        if (path.match(echoPath)) {
+            socket.write("Content-Type: text/plain\r\n");
+            const [,,content] = path.split('/');
+            socket.write(`Content-Length: ${content.length}\r\n\r\n${content}`);
+        }
+
         socket.end();
-        socket.destroy();
     });
 });
 
